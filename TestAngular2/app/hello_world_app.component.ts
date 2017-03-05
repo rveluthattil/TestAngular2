28ad48e20a805503d343d450624000c8a62ebd25
ram-synechron <Ramachandran.Veluthattil@ingramcontent.com>
2017-03-05 00:00:33 -0600

    initial commit

---
+++ TestAngular2/app/hello_world_app.component.ts
@@ -0,0 +1,12 @@
+﻿import {Component, View} from "angular2/core";
+
+@Component({
+    selector: 'my-app'
+})
+
+@View({
+    template: '<h2>Hello World</h2>'
+})
+
+export class MyHelloWorldClass {
+}
